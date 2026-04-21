2459516f7f17021c4443e9ef9511bd2ac1c3ab7f
Gurpreet <gurpreet.dev92@gmail.com>
2026-04-21 17:29:04 +0530

    change

--- app/Filament/Resources/PosOrders/Pages/CreatePosOrder.php
+++ app/Filament/Resources/PosOrders/Pages/CreatePosOrder.php
@@ -16,22 +16,4 @@ class CreatePosOrder extends CreateRecord
         app(ReservationFolioService::class)->syncPosOrderCharges($this->record->fresh(['reservation', 'reservationRoom']));
     }
 
-     protected function mutateFormDataBeforeCreate(array $data): array
-    {
-       foreach ($data['items'] as &$item) {
-
-            if (!isset($item['tax_percentage']) || $item['tax_percentage'] === null) {
-
-                if (!empty($item['tax_id'])) {
-                    $tax = \App\Models\Tax::find($item['tax_id']);
-                    $item['tax_percentage'] = $tax?->percentage ?? 0;
-                } else {
-                    $item['tax_percentage'] = 0;
-                }
-            }
-        }
-
-        return $data;
-    }
-
 }
